b51ccb7d850a2ea0207dac032887bc4a33fa4768
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-08-27 16:23:37 +0200

    add managing register, connexion and crud in the 'user.php' file

--- User.php
+++ User.php
@@ -1,66 +1,59 @@
 <?php
-class User
-{
-    private $id;
-    public $login;
-    public $email;
-    public $firstname;
-    public $lastname;
+class User {
     private $conn;
 
-    // Constructeur pour initialiser la connexion à la base de données
-    public function __construct($servername, $username, $password, $dbname)
-    {
+    public function __construct($servername, $username, $password, $dbname) {
         $this->conn = new mysqli($servername, $username, $password, $dbname);
-
+        
         if ($this->conn->connect_error) {
             die("Connexion échouée: " . $this->conn->connect_error);
         }
     }
 
-    // Méthode pour créer un nouvel utilisateur
-    public function create($login, $password, $email, $firstname, $lastname)
-    {
+    public function register($login, $password, $email, $firstname, $lastname) {
         $stmt = $this->conn->prepare("INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)");
-
         if (!$stmt) {
             die("Erreur de préparation : " . $this->conn->error);
         }
-
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
         $stmt->bind_param("sssss", $login, $hashed_password, $email, $firstname, $lastname);
+        return $stmt->execute();
+    }
 
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            die("Erreur d'exécution : " . $stmt->error);
+    public function login($login, $password) {
+        $stmt = $this->conn->prepare("SELECT * FROM utilisateurs WHERE login = ?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows == 1) {
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['password'])) {
+                return $user;
+            }
         }
+        return null;
     }
 
-    // Méthode pour récupérer l'ID du dernier enregistrement inséré
-    public function getLastInsertedId()
-    {
-        return $this->conn->insert_id;
-    }
-
-    // Méthode pour lire un utilisateur
-    public function read($id)
-    {
+    public function getUserById($id) {
         $stmt = $this->conn->prepare("SELECT * FROM utilisateurs WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            return $result->fetch_assoc();
-        } else {
-            return null;
-        }
+        return $stmt->get_result()->fetch_assoc();
     }
 
-    // Méthode pour fermer la connexion
-    public function __destruct()
-    {
+    public function update($id, $login, $email, $firstname, $lastname) {
+        $stmt = $this->conn->prepare("UPDATE utilisateurs SET login = ?, email = ?, firstname = ?, lastname = ? WHERE id = ?");
+        $stmt->bind_param("ssssi", $login, $email, $firstname, $lastname, $id);
+        return $stmt->execute();
+    }
+
+    public function delete($id) {
+        $stmt = $this->conn->prepare("DELETE FROM utilisateurs WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        return $stmt->execute();
+    }
+
+    public function __destruct() {
         $this->conn->close();
     }
 }
