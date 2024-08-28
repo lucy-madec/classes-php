a9d256b73d7c1d56b2debf6b4d72d9db1dca7783
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-08-28 09:24:40 +0200

    adding the css link in the head of the html

--- dashboard.php
+++ dashboard.php
@@ -37,6 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de Bord</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
